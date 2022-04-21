c30825d1a5812872a8b44d71732b22fc4da08b62
Justin Uding <54405777+Ferbie10@users.noreply.github.com>
2022-04-21 07:13:25 -0500

    Add files via upload

---
+++ Tester4Scrapper.py
@@ -0,0 +1,28 @@
+
+
+finalList= {'turtle':['turtle10']}
+
+keyList = ['turtle','cat','mouse','dog']
+valueList =['turtle1','turtle2','turtle1','cat1','cat2','dog','dog1']
+for i in keyList:
+        finalList[i]= []
+
+for items in finalList.keys():
+    #print('***Keys***'+items)
+    for elements in valueList:
+        #print('***Values***'+elements)
+        res = not any(finalList.values())
+        if items in elements:
+                
+            if elements not in finalList[items]:
+                finalList[items].append(elements)
+
+                
+                
+            
+
+        
+
+print(finalList)
+#finalList[items].append(elements)
+#print('VALUE LIST: '+valueList.values())
