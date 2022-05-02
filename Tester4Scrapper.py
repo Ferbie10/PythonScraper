f24bd1ee30bf1aaf07ae69bc6b70ea04ae3547ec
Justin Uding <54405777+Ferbie10@users.noreply.github.com>
2022-05-02 06:58:29 -0500

    Add files via upload

--- Tester4Scrapper.py
+++ Tester4Scrapper.py
@@ -1,30 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+from collections import Counter
+import os
+
+class Web_Search:
+
+    def __init__(self,URL):
+        self.URL = URL
+    def webSeach(self):
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
+        source=requests.get(self.URL, headers=headers)
+        soup = BeautifulSoup(source.text, "html.parser")
+        hrefList = []
+        for i in soup.findAll('a', href=True):
+        # find all href on html web page
+            test = (i['href'])
+        # add all href to hrefList
+            hrefList.append(test)
+        return(hrefList)
+
+class Web_Drill:
+    def __init__(self,hrefList):
+        self.hrefList = hrefList
+
+    def webDrill(self):
+        for items in self.hrefList:
+            item = str(items)
+            hrefLists = []
+            substring = 'beavertools'
+            if substring in item:
+                try:
+                    url = Web_Search(item)
+                    hrefLists = url.webSeach()
+                    for i in hrefLists:
+                        if i not in hrefLists:
+                           hrefLists.append(i)
+                except:
+                    print(item)
+                            
+        return(hrefLists)
 
 
-finalList= {'turtle':['turtle10']}
 
-keyList = ['turtle','cat','mouse','dog']
-valueList =['turtle1','turtle2','turtle1','cat1','cat2','dog','dog1']
-for i in keyList:
-        finalList[i]= []
 
-for items in finalList.keys():
-    #print('***Keys***'+items)
-    for elements in valueList:
-        #print('***Values***'+elements)
-        res = not any(finalList.values())
-        if items in elements:
-                
-            if elements not in finalList[items]:
-                finalList[items].append(elements)
+def main():
+    userURL = 'https://beavertools.com/'
+    try:
+        url = Web_Search(userURL)
+        hrefList = url.webSeach()
+    except:
+        print('You have entered an invaild URL. Please try again:')
+    url1 = Web_Drill(hrefList)
+    hrefLists = url1.webDrill()
+    print(len(hrefList))
+    print(len(hrefLists))
+   
 
-                
-for key, value in finalList.items():
-    for i in value:
-        print(key,' : ', i)
-            
-
-        
-
-print(finalList)
-#finalList[items].append(elements)
-#print('VALUE LIST: '+valueList.values())
+main()
